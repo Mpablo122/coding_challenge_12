ec81568ed42987bb23feee94b800adc41c4f6b8c
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-03-02 17:30:19 -0500

    fully updated

--- cc_12.js
+++ cc_12.js
@@ -3,15 +3,27 @@ const dashboard_1 = document.getElementById("dashboard");
 const dashboard_2 = document.querySelector("#dashboard");
 // selecting the dashboard container using the getelementid and queryselector 
 const revenueCard = document.createElement("div");
+const profitCard = document.createElement("div");
+const expensesCard = document.createElement("div");
 
 revenueCard.setAttribute("class","metric-card");
 revenueCard.setAttribute("id", "revenueCard");
 
+profitCard.setAttribute("class","metric-card");
+profitCard.setAttribute("id", "profitCard");
+
+expensesCard.setAttribute("class","metric-card");
+expensesCard.setAttribute("id", "expensesCard");
+
 // making metric card div
 revenueCard.innerHTML = "<h3>Revenue</h3><p>$0</p>"
+profitCard.innerHTML = "<h3>Profit</h3><p>$0</p>"
+expensesCard.innerHTML = "<h3>Expenses</h3><p>$0</p>"
 
 // making sure the attributes are set for the class as well as for the id
 dashboard_1.appendChild(revenueCard);
+dashboard_1.appendChild(profitCard);
+dashboard_1.appendChild(expensesCard);
 
 
 // "Task 2 - Updated Metric Cards via Array Conversion"
