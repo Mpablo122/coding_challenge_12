3aff8c93bb62961c34f995ce536b3a6650957a3c
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-03-02 00:48:33 -0500

    "Task 2 - Updated Metric Cards via Array Conversion"

--- cc_12.js
+++ cc_12.js
@@ -10,3 +10,12 @@ revenueCard.setAttribute("id", "revenueCard");
 revenueCard.innerHTML = "<h3>Revenue</h3><p>$0</p>"
 
 dashboard_1.appendChild(revenueCard);
+
+// "Task 2 - Updated Metric Cards via Array Conversion"
+
+const metricCards = document.querySelectorAll(".metric-card");
+
+Array.from(metricCards).forEach(card =>{
+    card.innerHTML += " - Updated";
+    card.style.backgroundColor = "lightgreen"
+});
